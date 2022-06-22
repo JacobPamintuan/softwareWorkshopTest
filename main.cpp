45877bfa98671e27c3adc9e71f2aa92821a034b1
JacobPamintuan <jcpamintu@gmail.com>
2022-06-22 11:51:34 -0400

    Adding from J comptuter

--- main.cpp
+++ main.cpp
@@ -4,4 +4,6 @@ int main(){
     std::cout <<"This is Great";
     std::cout << "haha";
     std::cout << "The Great workshop~~~~~~~~~~~~~~~";
+
+    std::cout << "Adding from different computer (J)";
 }
